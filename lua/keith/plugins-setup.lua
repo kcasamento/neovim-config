8ea3fd0216c9d276a78255a106419a89d5bc9ba6
Keith Casamento <kcasamento@rvohealth.com>
2023-01-24 10:21:37 -0500

    adding markdown preview

--- lua/keith/plugins-setup.lua
+++ lua/keith/plugins-setup.lua
@@ -268,6 +268,13 @@ return packer.startup(function(use)
     end
   }
 
+  use{
+    "iamcco/markdown-preview.nvim",
+    run = "cd app && npm install",
+    setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
+    ft = { "markdown" },
+  }
+
 
   if packer_bootstrap then
     require('packer').sync()
